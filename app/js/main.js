3e2f8bec229d38e6320e9aa46905038b9c101ed9
YakDm79 <gav1@ya.ru>
2021-07-14 17:48:16 +0300

    слайдер на главной

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,24 @@
 $('.header__menu-burger').on('click', function() {
     $('.header__menu-list').toggleClass('header__menu-list--active');
 });
+$('.slider-main').slick({
+    arrows: false,
+    vertical: true,
+    // rows: 1,
+    // slidesPerRow: 1,
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 6000,
+    slidesToShow: 3,
+    infinite: true,
+});
+$('.slider-main-sec').slick({
+    arrows: false,
+    vertical: true,
+    // rows: 1,
+    // slidesPerRow: 1,
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 6000,
+    slidesToShow: 3
+});
